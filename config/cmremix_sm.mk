0c80eade90e5181c624258a8f350bd93bbe39ccf
Paul Beeler <pbeeler80@gmail.com>
2015-01-25 17:55:47 -0700

    Move flags and disable modules lists out of build (2/2)
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    (cherry picked from commit abb0ad3a935a39203686e5085d35e38bd209b39b)
    
    Conflicts:
    	config/sm.mk

--- config/cmremix_sm.mk
+++ config/cmremix_sm.mk
@@ -1,3 +1,18 @@
+# Copyright (C) 2014-2015 The SaberMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Written for SaberMod toolchains
 # Find host os
 
@@ -11,77 +26,216 @@ ifeq (Linux,$(UNAME))
 endif
 
 # Only use these compilers on linux host.
-ifeq (linux,$(HOST_OS))
+ifeq ($(strip $(HOST_OS)),linux)
+  ifndef TARGET_ARCH
+    $(warning ********************************************************************************)
+    $(warning *  TARGET_ARCH not defined.)
+    $(warning *  This is commonly set in device trees BoardConfig.mk.)
+    $(warning *  Define TARGET_ARCH before including this file sm.mk)
+    $(warning ********************************************************************************)
+  endif
 
 # Add extra libs for the compilers to use
 # Filter by TARGET_ARCH since we're pointing to ARCH specific compilers.
 # To use this on new devices define TARGET_ARCH in device makefile.
-ifeq (arm,$(TARGET_ARCH))
-export LD_LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
-export LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
+  ifeq ($(strip $(TARGET_ARCH)),arm)
 
-# Path to toolchain
-SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8
-SM_AND := $(shell $(SM_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
+    # Add extra libs for the compilers to use
+    export LD_LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
+    export LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8/arch-arm/usr/lib
 
-# Find strings in version info
-ifneq ($(filter (SaberMod%),$(SM_AND)),)
-SM_AND_NAME := $(filter (SaberMod%),$(SM_AND))
-SM_AND_DATE := $(filter 20140% 20141% 20150% 20151%,$(SM_AND))
-SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
-SM_AND_VERSION := $(SM_AND_NAME)-$(SM_AND_DATE)-$(SM_AND_STATUS)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sm.android=$(SM_AND_VERSION)
-endif
 
-SM_KERNEL_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-4.9
-SM_KERNEL := $(shell $(SM_KERNEL_PATH)/bin/arm-eabi-gcc --version)
+    # Path to ROM toolchain
+    SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.8
+    SM_AND := $(shell $(SM_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
 
-ifneq ($(filter (SaberMod%),$(SM_KERNEL)),)
-SM_KERNEL_NAME := $(filter (SaberMod%),$(SM_KERNEL))
-SM_KERNEL_DATE := $(filter 20140% 20141% 20150% 20151%,$(SM_KERNEL))
-SM_KERNEL_STATUS := $(filter (release) (prerelease) (experimental),$(SM_KERNEL))
-SM_KERNEL_VERSION := $(SM_KERNEL_NAME)-$(SM_KERNEL_DATE)-$(SM_KERNEL_STATUS)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sm.kernel=$(SM_KERNEL_VERSION)
-endif
+    # Find strings in version info
+    ifneq ($(filter (SaberMod%),$(SM_AND)),)
+      SM_AND_NAME := $(filter (SaberMod%),$(SM_AND))
+      SM_AND_DATE := $(filter 20140% 20141% 20150% 20151%,$(SM_AND))
+      SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
+      SM_AND_VERSION := $(SM_AND_NAME)-$(SM_AND_DATE)-$(SM_AND_STATUS)
 
-ifeq (true,$(GRAPHITE_OPTS))
-OPT1 := (graphite)
-endif
-endif
+      PRODUCT_PROPERTY_OVERRIDES += \
+        ro.sm.android=$(SM_AND_VERSION)
+    endif
 
-ifeq (arm64,$(TARGET_ARCH))
-export LD_LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
-export LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
+    # Path to kernel toolchain
+    SM_KERNEL_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-4.9
+    SM_KERNEL := $(shell $(SM_KERNEL_PATH)/bin/arm-eabi-gcc --version)
 
-# Path to toolchain
-SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
-SM_AND := $(shell $(SM_AND_PATH)/bin/aarch64-linux-android-gcc --version)
+    ifneq ($(filter (SaberMod%),$(SM_KERNEL)),)
+      SM_KERNEL_NAME := $(filter (SaberMod%),$(SM_KERNEL))
+      SM_KERNEL_DATE := $(filter 20140% 20141% 20150% 20151%,$(SM_KERNEL))
+      SM_KERNEL_STATUS := $(filter (release) (prerelease) (experimental),$(SM_KERNEL))
+      SM_KERNEL_VERSION := $(SM_KERNEL_NAME)-$(SM_KERNEL_DATE)-$(SM_KERNEL_STATUS)
+      PRODUCT_PROPERTY_OVERRIDES += \
+        ro.sm.kernel=$(SM_KERNEL_VERSION)
+    endif
 
-# Find strings in version info
-ifneq ($(filter (SaberMod%),$(SM_AND)),)
-SM_AND_NAME := $(filter (SaberMod%),$(SM_AND))
-SM_AND_DATE := $(filter 20140% 20141% 20150% 20151%,$(SM_AND))
-SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
-SM_AND_VERSION := $(SM_AND_NAME)-$(SM_AND_DATE)-$(SM_AND_STATUS)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sm.android=$(SM_AND_VERSION)
-endif
+  OPT1 := (graphite)
 
-ifeq (true,$(GRAPHITE_OPTS))
-OPT1 := (graphite)
-endif
-endif
-endif
+  # Graphite flags and friends
+  export GRAPHITE_FLAGS := \
+           -fgraphite \
+           -fgraphite-identity \
+           -floop-flatten \
+           -floop-parallelize-all \
+           -ftree-loop-linear \
+           -floop-interchange \
+           -floop-strip-mine \
+           -floop-block \
+           -Wno-error=maybe-uninitialized
 
-ifeq (true,$(STRICT_ALIASING))
-OPT2 := (strict)
-endif
+  # Force disable some modules that are not compatible with graphite flags
+  LOCAL_DISABLE_GRAPHITE := \
+    libunwind \
+    libFFTEm \
+    libicui18n \
+    libskia \
+    libvpx \
+    libmedia_jni \
+    libstagefright_mp3dec \
+    libart \
+    mdnsd \
+    libwebrtc_spl \
+    third_party_WebKit_Source_core_webcore_svg_gyp
+  endif
 
-ifeq (true,$(USE_O3_OPTIMIZATIONS))
-OPT3 := (extreme)
-endif
+  ifeq ($(strip $(TARGET_ARCH)),arm64)
+
+    # Add extra libs for the compilers to use
+    export LD_LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
+    export LIBRARY_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/arch-arm64/usr/lib
+
+
+    # Path to toolchain
+    SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9
+    SM_AND := $(shell $(SM_AND_PATH)/bin/aarch64-linux-android-gcc --version)
+
+    # Find strings in version info
+    ifneq ($(filter (SaberMod%),$(SM_AND)),)
+      SM_AND_NAME := $(filter (SaberMod%),$(SM_AND))
+      SM_AND_DATE := $(filter 20140% 20141% 20150% 20151%,$(SM_AND))
+      SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
+      SM_AND_VERSION := $(SM_AND_NAME)-$(SM_AND_DATE)-$(SM_AND_STATUS)
+      PRODUCT_PROPERTY_OVERRIDES += \
+        ro.sm.android=$(SM_AND_VERSION)
+    endif
+
+  OPT1 := (graphite)
+
+  # Graphite flags and friends
+  export GRAPHITE_FLAGS := \
+           -fgraphite \
+           -fgraphite-identity \
+           -floop-flatten \
+           -floop-parallelize-all \
+           -ftree-loop-linear \
+           -floop-interchange \
+           -floop-strip-mine \
+           -floop-block \
+           -Wno-error=maybe-uninitialized
+
+  # Force disable some modules that are not compatible with graphite flags
+  LOCAL_DISABLE_GRAPHITE := \
+    libunwind \
+    libFFTEm \
+    libicui18n \
+    libskia \
+    libvpx \
+    libmedia_jni \
+    libstagefright_mp3dec \
+    libart \
+    mdnsd \
+    libwebrtc_spl \
+    third_party_WebKit_Source_core_webcore_svg_gyp
+  endif
+
+  ifeq ($(strip $(STRICT_ALIASING)),true)
+  OPT2 := (strict)
+
+  LOCAL_DISABLE_STRICT := \
+	libc_bionic \
+	libc_dns \
+	libc_tzcode \
+	libziparchive \
+	libtwrpmtp \
+	libfusetwrp \
+	libguitwrp \
+	busybox \
+	libuclibcrpc \
+	libziparchive-host \
+	libpdfiumcore \
+	libandroid_runtime \
+	libmedia \
+	libpdfiumcore \
+	libpdfium \
+	bluetooth.default \
+	logd \
+	mdnsd \
+	net_net_gyp \
+	libstagefright_webm \
+	libaudioflinger \
+	libmediaplayerservice \
+	libstagefright \
+	ping \
+	ping6 \
+	libdiskconfig \
+	libjavacore \
+	libfdlibm \
+	libvariablespeed \
+	librtp_jni \
+	libwilhelm \
+	libdownmix \
+	libldnhncr \
+	libqcomvisualizer \
+	libvisualizer \
+	libstlport \
+	libutils \
+	libandroidfw \
+	dnsmasq \
+	static_busybox \
+	libwebviewchromium \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support \
+	content_content_renderer_gyp \
+	third_party_WebKit_Source_modules_modules_gyp \
+	third_party_WebKit_Source_platform_blink_platform_gyp \
+	third_party_WebKit_Source_core_webcore_remaining_gyp \
+	third_party_angle_src_translator_lib_gyp \
+	third_party_WebKit_Source_core_webcore_generated_gyp \
+	libc_gdtoa \
+	libc_openbsd \
+	libc \
+	libc_nomalloc \
+	libc_malloc \
+	camera.msm8084 \
+	libfusetwrp \
+	libguitwrp \
+	libwnndict \
+	libtwrpmtp \
+	libstlport_static \
+	gatt_testtool \
+	libfuse \
+	lsof \
+	libOmxVenc
+  endif
+
+  ifeq ($(strip $(O3_OPTIMIZATIONS)),true)
+    OPT3 := (extreme)
+
+    # Disable some modules that break with -O3
+    LOCAL_DISABLE_O3 := \
+      libaudioflinger \
+      libwebviewchromium
+
+    # -O3 flags and friends
+    O3_FLAGS := \
+      -O3 \
+      -Wno-error=array-bounds \
+      -Wno-error=strict-overflow
+  endif
 
 GCC_OPTIMIZATION_LEVELS := $(OPT1)$(OPT2)$(OPT3)
 ifneq (,$(GCC_OPTIMIZATION_LEVELS))
@@ -89,3 +243,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sm.flags=$(GCC_OPTIMIZATION_LEVELS)
 endif
 
+else
+  $(warning ********************************************************************************)
+  $(warning *  SaberMod currently only works on linux host systems.)
+  $(warning ********************************************************************************)
+endif
