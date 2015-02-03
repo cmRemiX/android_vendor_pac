b33fdeffe596af5fb11fddc0f8509f5540c9a303
ZION959 <ziontran@gmail.com>
2015-02-02 17:23:51 -0700

    build cmRemiX v6.9

--- tools/build-cmremix.sh
+++ tools/build-cmremix.sh
@@ -2,7 +2,7 @@
 
 # cmRemiX version System
 export CMREMIX_VERSION_MAJOR="LP"
-export CMREMIX_VERSION_MINOR="v6.4"
+export CMREMIX_VERSION_MINOR="v6.9"
 export CMREMIX_VERSION_MAINTENANCE="ZION959"
 # Acceptible maitenance versions are; Stable, Dev, Nightly
 
@@ -115,7 +115,6 @@ export LOCAL_LTO=true
 # export FLOOP_NEST_OPTIMIZE=true
 export USE_PREBUILT_CHROMIUM=1
 export USE_CCACHE=1
-export CMREMIX_CHANGELOG=true
 
 opt_adb=0
 opt_chromium=0
