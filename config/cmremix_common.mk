241063c9e5f7840fffe4e6c221b781d97988de77
José Adolfo Galdámez <josegalre@gmail.com>
2015-03-03 14:42:30 -0700

    vendor: optimizations for devices with low-RAM [2/2]
    Credit & Thanks to PAC Team
    
    * Remove some apps based on:
      https://gerrit.omnirom.org/#/c/11993
      https://gerrit.omnirom.org/#/c/12046
    
    * Reference for low-ram devices: https://source.android.com/devices/tech/low-ram.html
    
    * Use dlmalloc instead of jemalloc, it performs better on low-ram devices
    
    * Reduce background apps limit to 16 on low-tier devices
    
    * Set max background services to 8 on low-tier devices
    
    * Disable bootanimation cached texture, saves memory used at boot
    
    * Disable atlas services on low-ram targets
    
    Flag: CMREMIX_LOW_RAM_DEVICE := true
    
    Add this in your BoardConfig or built using -l
    
    Change-Id: I50f43774695847ccec03d7743b6454937a168307
    
    Conflicts:
    	config/pac_common.mk

--- config/cmremix_common.mk
+++ config/cmremix_common.mk
@@ -79,6 +79,20 @@ CMREMIX_BUILD_VERSION := cmRemiX-$(BOARD)-$(CMREMIXVERSION)-$(shell date +%Y%m%d
 # Set the board version
 CM_BUILD := $(BOARD)
 
+# Lower RAM devices
+ifeq ($(CMREMIX_LOW_RAM_DEVICE),true)
+MALLOC_IMPL := dlmalloc
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    config.disable_atlas=true \
+    dalvik.vm.jit.codecachesize=0 \
+    persist.sys.force_highendgfx=true \
+    ro.config.low_ram=true \
+    ro.config.max_starting_bg=8 \
+    ro.sys.fw.bg_apps_limit=16
+endif
+
 # ROMStats Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.cmremixstats.name=cmRemix Rom \
