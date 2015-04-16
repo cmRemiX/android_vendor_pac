e8cded6f57eb684b957bb2025e3e898c41050092
ZION959 <ziontran@gmail.com>
2015-04-16 11:31:03 -0700

    Saberize for all support device

--- products/cmremix_hltespr.mk
+++ products/cmremix_hltespr.mk
@@ -1,48 +1,8 @@
 # Check for target product
 ifeq (cmremix_hltespr,$(TARGET_PRODUCT))
 
-# Target arch is arm
-TARGET_ARCH := arm
-
-# Some common sabermod variables before common
-TARGET_SM_AND := 4.8
-TARGET_SM_KERNEL := 4.9
-TARGET_LIB_VERSION := 4.8
-
-# Allow overriding of NDK toolchain version
-TARGET_NDK_VERSION := 4.8
-
-# Set Custom GCC Kernel Version 
-TARGET_GCC_VERSION_ARM := 4.9
-
-# Set Custom GCC Rom Version 
-TARGET_GCC_VERSION_AND := 4.8
-
-# ignore recovery size build error!
-TARGET_NO_RECOVERY := true
-#CMREMIX_IGNORE_RECOVERY_SIZE := true
-
-# Set Qcom Clang 3.5 
-USE_CLANG_QCOM := true
-#USE_CLANG_QCOM_LTO := true
-LTO_OPTIMIZATION := true
-
-# Enabled SaberMod Optimization Here
-CMREMIX_OPTIMIZATIONS := true
-GRAPHITE_OPTS := true
-STRICT_ALIASING := true
-USE_HOST_4_8 := true
-O3_OPTIMIZATIONS := true
-KRAIT_TUNINGS := true
-ENABLE_GCCONLY := true
-TARGET_USE_PIPE := true
-ENABLE_PTHREAD := true
-FLOOP_NEST_OPTIMIZE := true
-
-# General flags for gcc 4.9 to allow compilation to complete.
-MAYBE_UNINITIALIZED := \
-
-##
+# Inherit sabermod device configuration
+include vendor/cmremix/products/sm_hltespr.mk
 
 # Set bootanimation Size
 CMREMIX_BOOTANIMATION_NAME := 1080
