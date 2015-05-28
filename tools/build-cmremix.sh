642f003306637d35882eb8474cfcc290f65dd43f
ZION959 <ziontran@gmail.com>
2015-05-27 17:48:15 -0700

    cmremix build script sync with PAC
    add build bootzip option
    
    Origin source link: https://github.com/PAC-man/android_vendor_pac/blob/pac-5.1/tools/build-pac.sh
    
    all Credit Pac Team Thank You
    
    Squash of follow commits
    
    vendor: Rainbow unicorn puke [2/2]
    
    Change the color palette to be more script and user friendly.
    Add most common colors and pairings.
    Recolor some output of PAC files.
    
    Removed total build time from build-pac.sh. The time difference
    is only a few seconds more than makes count anyways.
    
    Made and added PAC ANSI splash.
    
    Changed Lokesh's "Do not release public builds!" warning to one
    XDA derps (and other derps) can better relate to.
    
    Rainbow unicorn poop
    
    https://github.com/PAC-man/android_vendor_pac/commit/104e3d23171925e1cdfa42dd32ee66708043ce74
    
    @MaDc0wUSP
    
    Change-Id: I96a645f81793caacfbba33b334f9df88fa74de73
    
     vendor: cleaning our files
    
    * Add our licence (thanks to Tom)
    * Add option to not use ccache
    * Add some double quote to prevent globbing and word splitting
    * Now use grep -c instead of grep|wc
    * Now use $(..) instead of legacy `..`
    
    p16: (apparently): copyleft text pursuant to Gnu.
    p17: headers: fixup build-pac.sh and whitespace derps.
    p18: fixup usage of USE_PREBUILT_CHROMIUM and USE_CCACHE
    p19: putting back @jaaga's changes I inadvertently overwrote
    p21: add colors for .mk files
    
    @JoseGalRe
    https://github.com/PAC-man/android_vendor_pac/commit/78d630c5658bccad00c53e804d503e9aa5467f06
    Change-Id: I27a0e0f96d71e4dbe15844bf0150831c685dee98
    
    Remove twrp options from main branch
    
    @Lokesh Chamane
    
    https://github.com/PAC-man/android_vendor_pac/commit/2c9358b2515b43c86111a8234e4059fb1b5e73c8
    
    Change-Id: I660e4966507af448f8162cacc03a437709391324
    
     build-pac.sh - Inform user build dir is being cleaned
    
    Currently, on my system at least, the process of make clean takes
    a little bit of time where it can seem that the script may have frozen.  Just
    insert a simple echo to inform the user what the script is doing.
    
    Feel free to change the colors. =p
    
    @my95z34
    
    Change-Id: Ibdaeccedf089365c8982dd79bdd0120acdb45626
    (cherry picked from commit 3032535)
    
    https://github.com/PAC-man/android_vendor_pac/commit/dd20f64b9eb266e4558f5a84b8f1112ea8cffc74
    
     tools: add "ignore minor errors" and "log warnings" options [1/2]
    
    [1/2]vendor/pac/tools/build-pac.sh
            [2/2]build/core/Makefile
    
           1.  Ignore minor errors during compilation to allow
               build to complete
           2.  Revise build options to allow for multiple choices
    
           3.  Werner Gerber <langes72@gmail.com> original work
    
               Option to send warnings and errors to a log file
                -w1: warnings
                -w2: all output
    
               Copy warnings and errors to $BUILD_TOP/build.log, while
               also showing them in terminal. The log file will be overwritten
               every time option is invoked.
      @ shumashv1
    
    https://github.com/PAC-man/android_vendor_pac/commit/7b6de9f857f70606f276c8bb24a53fb6ad4b94a5
    Change-Id: Ica18eedcce85a97575a367fad22eb0dafad02c33
    
     build-pac.sh - Because grammar.
    
    =p
    @my95z34
    
    https://github.com/PAC-man/android_vendor_pac/commit/9bdfc68a053f3c66130ab5744bb944fa7a839aaf
    
    Change-Id: Ia486ebbc27b032952b14331f687040e176429be9
    
     vendor: Fix up copyright headers
    
    https://github.com/PAC-man/android_vendor_pac/commit/71d20675d5a813475d706e05420b1b85d1c27da6
    
    Change-Id: Ie0d619674ea74e66557a6b9c26cd4630691dab9f
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- tools/build-cmremix.sh
+++ tools/build-cmremix.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
+#
+# build-pac.sh: the overarching build script for the ROM.
+# Copyright (C) 2015 The PAC-ROM Project
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 
-# CM-Remix version System
-export CMREMIX_VERSION_MAJOR="LP"
-export CMREMIX_VERSION_MINOR="5.1.1"
-export CMREMIX_VERSION_MAINTENANCE="Unofficial"
-# Acceptible maintenance versions are; Stable, Official, Nightly or Unofficial
-
-# CM-Remix Version Logic
-if [ -s ~/CMREMIXname ]; then
-    export CMREMIX_MAINTENANCE=$(cat ~/CMREMIXname)
-else
-    export CMREMIX_MAINTENANCE="$CMREMIX_VERSION_MAINTENANCE"
-fi
-export CMREMIX_VERSION="$CMREMIX_VERSION_MAJOR $CMREMIX_VERSION_MINOR $CMREMIX_MAINTENANCE"
-
-usage()
-{
-    echo -e ""
-    echo -e "${bldwhi}Usage:${rst}"
+usage() {
+    echo -e "${bldblu}Usage:${bldcya}"
     echo -e "  build-cmremix.sh [options] device"
-    echo -e ""
-    echo -e "${bldwhi}  Options:${rst}"
+    echo ""
+    echo -e "${bldblu}  Options:${bldcya}"
     echo -e "    -a  Disable ADB authentication and set root access to Apps and ADB"
     echo -e "    -b# Prebuilt Chromium options:"
     echo -e "        1 - Remove"
@@ -28,6 +30,7 @@ usage()
     echo -e "    -c# Cleaning options before build:"
     echo -e "        1 - Run make clean"
     echo -e "        2 - Run make installclean"
+    echo -e "    -d  Build rom without ccache"
     echo -e "    -e# Extra build output options:"
     echo -e "        1 - Verbose build output"
     echo -e "        2 - Quiet build output"
@@ -35,6 +38,7 @@ usage()
     echo -e "    -j# Set number of jobs"
     echo -e "    -l  Optimizations for devices with low-RAM"
     echo -e "    -k  Rewrite roomservice after dependencies update"
+    echo -e "    -i  Ignore minor errors during build"
     echo -e "    -r  Reset source tree before build"
     echo -e "    -s# Sync options before build:"
     echo -e "        1 - Normal sync"
@@ -42,125 +46,165 @@ usage()
     echo -e "        3 - Restore previous snapshot, then snapshot sync"
     echo -e "    -o# Only build:"
     echo -e "        1 - Boot Image"
-    echo -e "        2 - Boot Zip"
+    echo -e "        1 - Boot Zip"
     echo -e "        3 - Recovery Image"
     echo -e "    -p  Build using pipe"
-    echo -e "    -t  Build ROM with TWRP Recovery (Extreme caution, ONLY for developers)"
-    echo -e "        (This may produce an invalid recovery. Use only if you have the correct settings for these)"
-    echo -e ""
-    echo -e "${bldwhi}  Example:${rst}"
-    echo -e "    ./build-cmremix.sh -c1 trltetmo"
-    echo -e ""
+    echo -e "    -w  Log file options:"
+    echo -e "        1 - Send warnings and errors to a log file"
+    echo -e "        2 - Send all output to a log file"
+    echo ""
+    echo -e "${bldblu}  Example:${bldcya}"
+    echo -e "    ./build-cmremix.sh -c1 shamu"
+    echo -e "${rst}"
     exit 1
 }
 
-# Colors
-. ./vendor/cmremix/tools/colors
 
+# Import Colors
+. ./vendor/cmremix/tools/colors
+. ./vendor/cmremix/tools/res/cmremix-start
+
+
+# CMREMIX version
+export CMREMIX_VERSION_MAJOR="LP"
+export CMREMIX_VERSION_MINOR="MR3"
+export CMREMIX_VERSION_MAINTENANCE="Unofficial"
+# Acceptable maintenance versions are; Stable, Official, Nightly or Unofficial
+
+
+# Default global variable values with preference to environmant.
+if [ -z "${USE_PREBUILT_CHROMIUM}" ]; then
+    export USE_PREBUILT_CHROMIUM=1
+fi
+if [ -z "${USE_CCACHE}" ]; then
+    export USE_CCACHE=1
+fi
+
+
+# Maintenance logic
+if [ -s ~/CMREMIXname ]; then
+    export CMREMIX_MAINTENANCE=$(cat ~/CMREMIXname)
+else
+    export CMREMIX_MAINTENANCE="$CMREMIX_VERSION_MAINTENANCE"
+fi
+export CMREMIX_VERSION="$CMREMIX_VERSION_MAJOR $CMREMIX_VERSION_MINOR $CMREMIX_MAINTENANCE"
+
+
+# Check directories
 if [ ! -d ".repo" ]; then
     echo -e "${bldred}No .repo directory found.  Is this an Android build tree?${rst}"
+    echo ""
     exit 1
 fi
 if [ ! -d "vendor/cmremix" ]; then
-    echo -e "${bldred}No vendor/cmremix directory found.  Is this a CM-Remix build tree?${rst}"
+    echo -e "${bldred}No vendor/cmremix directory found.  Is this a CMREMIX build tree?${rst}"
+    echo ""
     exit 1
 fi
+
 
 # Figure out the output directories
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 thisDIR="${PWD##*/}"
 
-findOUT() {
 if [ -n "${OUT_DIR_COMMON_BASE+x}" ]; then
-return 1; else
-return 0
-fi;}
+    RES=1
+else
+    RES=0
+fi
 
-findOUT
-RES="$?"
 
 if [ $RES = 1 ];then
     export OUTDIR=$OUT_DIR_COMMON_BASE/$thisDIR
-    echo -e ""
-    echo -e "${cya}External out directory is set to: ($OUTDIR)${rst}"
-    echo -e ""
+    echo -e "${bldcya}External out directory is set to: ${bldgrn}($OUTDIR)${rst}"
+    echo ""
 elif [ $RES = 0 ];then
     export OUTDIR=$DIR/out
-    echo -e ""
-    echo -e "${cya}No external out, using default: ($OUTDIR)${rst}"
-    echo -e ""
+    echo -e "${bldcya}No external out, using default: ${bldgrn}($OUTDIR)${rst}"
+    echo ""
 else
-    echo -e ""
-    echo -e "${bldred}NULL${rst}"
-    echo -e "${bldred}Error, wrong results! Blame the split screen!${rst}"
-    echo -e ""
+    echo -e "${bldred}NULL"
+    echo -e "Error, wrong results! Blame the split screen!${rst}"
+    echo ""
 fi
+
 
 # Get OS (Linux / Mac OS X)
 IS_DARWIN=$(uname -a | grep Darwin)
 if [ -n "$IS_DARWIN" ]; then
     CPUS=$(sysctl hw.ncpu | awk '{print $2}')
-    DATE=gdate
 else
-    CPUS=$(grep "^processor" /proc/cpuinfo | wc -l)
-    DATE=date
+    CPUS=$(grep "^processor" /proc/cpuinfo -c)
 fi
 
-export USE_CCACHE=1
 
 opt_adb=0
 opt_chromium=0
 opt_clean=0
+opt_ccache=0
 opt_extra=0
 opt_fetch=0
 opt_jobs="$CPUS"
 opt_kr=0
+opt_ignore=0
 opt_lrd=0
 opt_only=0
 opt_pipe=0
 opt_reset=0
 opt_sync=0
-opt_twrp=0
+opt_log=0
 
-while getopts "ab:c:e:fj:klo:prs:t" opt; do
+
+while getopts "ab:c:de:fj:kilo:prs:w:" opt; do
     case "$opt" in
     a) opt_adb=1 ;;
     b) opt_chromium="$OPTARG" ;;
     c) opt_clean="$OPTARG" ;;
+    d) opt_ccache=1 ;;
     e) opt_extra="$OPTARG" ;;
     f) opt_fetch=1 ;;
     j) opt_jobs="$OPTARG" ;;
     k) opt_kr=1 ;;
+    i) opt_ignore=1 ;;
     l) opt_lrd=1 ;;
     o) opt_only="$OPTARG" ;;
     p) opt_pipe=1 ;;
     r) opt_reset=1 ;;
     s) opt_sync="$OPTARG" ;;
-    t) opt_twrp=1 ;;      
+    w) opt_log="$OPTARG" ;;
     *) usage
     esac
 done
+
 shift $((OPTIND-1))
 if [ "$#" -ne 1 ]; then
     usage
 fi
 device="$1"
 
-echo -e "$Building ${bldylw}CM-Remix-ROM ${bldmag}$CMREMIX_VERSION_MAJOR${rst} ${bldcya}$CMREMIX_VERSION_MINOR ${bldred}$CMREMIX_MAINTENANCE${rst}"
 
-if [ "$opt_chromium" -eq 1 ]; then
-    rm -rf prebuilts/chromium/$device
-    echo -e ""
-    echo -e "${bldcya}Prebuilt Chromium for $device removed${rst}"
-elif [ "$opt_chromium" -eq 2 ]; then
-    unset USE_PREBUILT_CHROMIUM
-    echo -e ""
-    echo -e "${bldcya}Prebuilt Chromium will not be used${rst}"
+# Ccache options
+if [ "$opt_ccache" -eq 1 ]; then
+    echo -e "${bldcya}Ccache not be used in this build${rst}"
+    unset USE_CCACHE
+    echo ""
 fi
 
+
+# Chromium options
+if [ "$opt_chromium" -eq 1 ]; then
+    rm -rf prebuilts/chromium/"$device"
+    echo -e "${bldcya}Prebuilt Chromium for $device removed${rst}"
+    echo ""
+elif [ "$opt_chromium" -eq 2 ]; then
+    unset USE_PREBUILT_CHROMIUM
+    echo -e "${bldcya}Prebuilt Chromium will not be used${rst}"
+    echo ""
+fi
+
+
 # CMREMIX device dependencies
-echo -e ""
-echo -e "${bldcya}Looking for CM-Remix product dependencies${rst}${cya}"
+echo -e "${bldcya}Looking for CMREMIX product dependencies${bldgrn}"
 if [ "$opt_kr" -ne 0 ]; then
     vendor/cmremix/tools/getdependencies.py "$device" "$opt_kr"
 else
@@ -168,134 +212,207 @@ else
 fi
 echo -e "${rst}"
 
-if [ "$opt_clean" -eq 1 ]; then
-    make clean >/dev/null
-    echo -e "${bldcya}Out is clean${rst}"
-    echo -e ""
-elif [ "$opt_clean" -eq 2 ]; then
-    . build/envsetup.sh && lunch "cmremix_$device-userdebug";
-    make installclean >/dev/null
-    echo -e "${bldcya}Out is dirty${rst}"
-    echo -e ""
-fi
 
-# TWRP Recovery
-if [ "$opt_twrp" -eq 1 ]; then
-    echo -e "${bldcya}TWRP Recovery will be built${rst}"
-    export RECOVERY_VARIANT=twrp
-    echo -e ""
+# Check if last build was made ignoring errors
+# Set if unset
+if [ -f ".ignore_err" ]; then
+   : ${TARGET_IGNORE_ERRORS:=$(cat .ignore_err)}
 else
-    unset RECOVERY_VARIANT
+   : ${TARGET_IGNORE_ERRORS:=false}
 fi
 
-# Disable ADB authentication and set root access to Apps and ADB
+export TARGET_IGNORE_ERRORS
+
+if [ "$TARGET_IGNORE_ERRORS" == "true" ]; then
+   opt_clean=1
+   echo -e "${bldred}Last build ignored errors. Cleaning Out${rst}"
+   unset TARGET_IGNORE_ERRORS
+   echo -e "false" > .ignore_err
+fi
+
+
+# Cleaning out directory
+if [ "$opt_clean" -eq 1 ]; then
+    echo -e "${bldcya}Cleaning output directory${rst}"
+    make clean >/dev/null
+    echo -e "${bldcya}Output directory is: ${bldgrn}Clean${rst}"
+    echo ""
+elif [ "$opt_clean" -eq 2 ]; then
+    . build/envsetup.sh
+    lunch "cmremix_$device-userdebug"
+    make installclean >/dev/null
+    echo -e "${bldcya}Output directory is: ${bldred}Dirty${rst}"
+    echo ""
+else
+    if [ -d "$OUTDIR/target" ]; then
+        echo -e "${bldcya}Output directory is: ${bldylw}Untouched${rst}"
+        echo ""
+    else
+        echo -e "${bldcya}Output directory is: ${bldgrn}Clean${rst}"
+        echo ""
+    fi
+fi
+
+
+# Disable ADB authentication
 if [ "$opt_adb" -ne 0 ]; then
     echo -e "${bldcya}Disabling ADB authentication and setting root access to Apps and ADB${rst}"
     export DISABLE_ADB_AUTH=true
-    echo -e ""
+    echo ""
 else
     unset DISABLE_ADB_AUTH
 fi
 
+
 # Lower RAM devices
 if [ "$opt_lrd" -ne 0 ]; then
-    echo -e ${bldblu}"Applying optimizations for devices with low RAM"${txtrst}
+    echo -e "${bldcya}Applying optimizations for devices with low RAM${rst}"
     export CMREMIX_LOW_RAM_DEVICE=true
-    echo -e ""
+    echo ""
 else
     unset CMREMIX_LOW_RAM_DEVICE
 fi
+
 
 # Reset source tree
 if [ "$opt_reset" -ne 0 ]; then
     echo -e "${bldcya}Resetting source tree and removing all uncommitted changes${rst}"
     repo forall -c "git reset --hard HEAD; git clean -qf"
-    echo -e ""
+    echo ""
 fi
+
 
 # Repo sync/snapshot
 if [ "$opt_sync" -eq 1 ]; then
     # Sync with latest sources
     echo -e "${bldcya}Fetching latest sources${rst}"
     repo sync -j"$opt_jobs"
-    echo -e ""
+    echo ""
 elif [ "$opt_sync" -eq 2 ]; then
     # Take snapshot of current sources
     echo -e "${bldcya}Making a snapshot of the repo${rst}"
-    repo manifest -o snapshot-$device.xml -r
-    echo -e ""
+    repo manifest -o snapshot-"$device".xml -r
+    echo ""
 elif [ "$opt_sync" -eq 3 ]; then
     # Restore snapshot tree, then sync with latest sources
     echo -e "${bldcya}Restoring last snapshot of sources${rst}"
-    echo -e ""
-    cp snapshot-$device.xml .repo/manifests/
+    echo ""
+    cp snapshot-"$device".xml .repo/manifests/
 
     # Prevent duplicate projects
     cd .repo/local_manifests
-      for file in *.xml ; do mv $file `echo $file | sed 's/\(.*\.\)xml/\1xmlback/'` ; done
+    for file in *.xml; do
+        mv "$file" "$(echo $file | sed 's/\(.*\.\)xml/\1xmlback/')"
+    done
 
-    cd $DIR
-    repo init -m snapshot-$device.xml
+    # Start snapshot file
+    cd "$DIR"
+    repo init -m snapshot-"$device".xml
     echo -e "${bldcya}Fetching snapshot sources${rst}"
+    echo ""
     repo sync -d -j"$opt_jobs"
+
+    # Prevent duplicate backups
     cd .repo/local_manifests
-      for file in *.xmlback ; do mv $file `echo $file | sed 's/\(.*\.\)xmlback/\1xml/'` ; done
-    echo -e ""
-    cd $DIR
-    rm -f .repo/manifests/snapshot-$device.xml
+    for file in *.xmlback; do
+        mv "$file" "$(echo $file | sed 's/\(.*\.\)xmlback/\1xml/')"
+    done
+
+    # Remove snapshot file
+    cd "$DIR"
+    rm -f .repo/manifests/snapshot-"$device".xml
     repo init
 fi
 
-rm -f $OUTDIR/target/product/$device/obj/KERNEL_OBJ/.version
 
-# Fetch cherry-picks
+# Fetch extras
 if [ "$opt_fetch" -ne 0 ]; then
-    ./vendor/cmremix/tools/cherries.sh $device
+    ./vendor/cmremix/tools/extras.sh "$device"
 fi
+
 
 # Setup environment
 echo -e "${bldcya}Setting up environment${rst}"
+echo -e "${bldmag}${line}${rst}"
 . build/envsetup.sh
+echo -e "${bldmag}${line}${rst}"
 
-# Remove system folder (This will create a new build.prop with updated build time and date)
-rm -f $OUTDIR/target/product/$device/system/build.prop
+
+# This will create a new build.prop with updated build time and date
+rm -f "$OUTDIR"/target/product/"$device"/system/build.prop
+
+# This will create a new .version for kernel version is maintained on one
+rm -f "$OUTDIR"/target/product/"$device"/obj/KERNEL_OBJ/.version
+
 
 # Lunch device
-echo -e ""
+echo ""
 echo -e "${bldcya}Lunching device${rst}"
-lunch "cmremix_$device-userdebug";
+lunch "cmremix_$device-userdebug"
 
-# Start compilation
+
+# Pipe option
 if [ "$opt_pipe" -ne 0 ]; then
     export TARGET_USE_PIPE=true
 else
     unset TARGET_USE_PIPE
 fi
 
-if [ "$opt_only" -eq 1 ]; then
-    echo -e "${bldcya}Starting compilation: ${cya}Only will be built Kernel Boot Image${rst}"
-    echo -e ""
-    make -j"$opt_jobs" bootimage
-elif [ "$opt_only" -eq 2 ]; then
-    echo -e "${bldcya}Starting compilation: ${cya}Only will be built Kernel Boot Zip${rst}"
-    echo -e ""
-    make -j"$opt_jobs" bootzip
-elif [ "$opt_only" -eq 3 ]; then
-    echo -e "${bldcya}Starting compilation: ${cya}Only will be built Recovery Image${rst}"
-    echo -e ""
-    make -j"$opt_jobs" recoveryimage
+
+# Get extra options for build
+if [ "$opt_extra" -eq 1 ]; then
+    opt_v=" "showcommands
+elif [ "$opt_extra" -eq 2 ]; then
+    opt_v=" "-s
 else
-    echo -e "${bldcya}Starting compilation${rst}"
-    echo -e ""
-    if [ "$opt_extra" -eq 1 ]; then
-        make -j"$opt_jobs" showcommands bacon
-    elif [ "$opt_extra" -eq 2 ]; then
-        make -j"$opt_jobs" -s bacon
+    opt_v=""
+fi
+
+
+# Ignore minor errors during build
+if [ "$opt_ignore" -eq 1 ]; then
+    opt_i=" "-k
+    export TARGET_IGNORE_ERRORS=true
+    echo -e "true" > .ignore_err
+    if [ "$opt_log" -eq 0 ]; then
+        opt_log=1
+    fi
+else
+    opt_i=""
+fi
+
+
+# Log file options
+if [ "$opt_log" -ne 0 ]; then
+    rm -rf build.log
+    if [ "$opt_log" -eq 1 ]; then
+        exec 2> >(sed -r 's/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g' | tee -a build.log)
     else
-        make -j"$opt_jobs" bacon
+        exec &> >(tee -a build.log)
     fi
 fi
 
+
+# Start compilation
+if [ "$opt_only" -eq 1 ]; then
+    echo -e "${bldcya}Starting compilation: ${bldgrn}Building Kernel Boot Image only${rst}"
+    echo ""
+    make -j$opt_jobs$opt_v$opt_i bootimage
+elif [ "$opt_only" -eq 2 ]; then
+    echo -e "${bldcya}Starting compilation: ${bldgrn}Building Kernel Boot Zip only${rst}"
+    echo ""
+    make -j$opt_jobs$opt_v$opt_i bootzip
+elif [ "$opt_only" -eq 3 ]; then
+    echo -e "${bldcya}Starting compilation: ${bldgrn}Building Recovery Image only${rst}"
+    echo ""
+    make -j$opt_jobs$opt_v$opt_i recoveryimage
+else
+    echo -e "${bldcya}Starting compilation: ${bldgrn}Building ${bldylw}CMREMIX-ROM ${bldmag}$CMREMIX_VERSION_MAJOR ${bldcya}$CMREMIX_VERSION_MINOR ${bldred}$CMREMIX_MAINTENANCE${rst}"
+    echo ""
+    make -j$opt_jobs$opt_v$opt_i bacon
+fi
+
+
 # Cleanup unused built
-rm -f $OUTDIR/target/product/$device/cm-*.*
-rm -f $OUTDIR/target/product/$device/cmremix_*-ota*.zip
+rm -f "$OUTDIR"/target/product/"$device"/cm-*.*
+rm -f "$OUTDIR"/target/product/"$device"/cmremix_*-ota*.zip
