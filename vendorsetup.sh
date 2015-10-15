dc849ff1d4d5b17c2687d58b74ed9631b7911f27
dario <darjwx>
2015-10-15 22:41:29 +0200

    Add huashan support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,4 +19,6 @@ add_lunch_combo cmremix_sprout4-userdebug
 add_lunch_combo cmremix_sprout8-userdebug
 # OnePlus One
 add_lunch_combo cmremix_bacon-userdebug
+# Sony Xperia SP
+add_lunch_combo cmremix_huashan-userdebug
 
