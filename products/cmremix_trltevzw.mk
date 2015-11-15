273bacf046e8e87b8ab002a806ef99fe516617d9
Hamada Badr <hamada.s.badr@gmail.com>
2015-11-15 10:11:33 -0500

    cmremix: add trltevzw to products

---
+++ products/cmremix_trltevzw.mk
@@ -0,0 +1,21 @@
+# Check for target product
+ifeq (cmremix_trltevzw,$(TARGET_PRODUCT))
+
+# Synapse 
+TARGET_ENABLE_UKM := true
+
+# Disable ADB authentication and set root access to Apps and ADB
+DISABLE_ADB_AUTH := true
+
+# Set bootanimation Size
+CMREMIX_BOOTANIMATION_NAME := 1440
+
+# Include CM-Remix common configuration
+include vendor/cmremix/config/cmremix_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/samsung/trltevzw/cm.mk)
+
+PRODUCT_NAME := cmremix_trltevzw
+
+endif
